45db6a2ba88c1bb41cc33f17342df11bc2859d59
okbrown <orlando.brown@stepstone.com>
2020-02-24 11:21:16 +0000

    add logos

--- src/middleware/jobs.js
+++ src/middleware/jobs.js
@@ -8,7 +8,7 @@ const jobs = {
 			company: "Acme Company",
 			post_date: "14 days ago",
 			description: "React Developer Circa £75,000 City of London - Marylebone (NW1) ? ShortList Recruitment … The React Developer will work on a re-platforming of existing products to take advantage … The React Developer will benefit from working in a purely greenfield project",
-			logo:""
+			logo:"https://logo.clearbit.com/company.com"
 		},
 		{
 			title: "Senior Developer",
@@ -18,7 +18,7 @@ const jobs = {
 			company: "Acme Company",
 			post_date: "14 days ago",
 			description: "Senior .NET Full Stack Developer - Reigate - Insurance - Azure Cloud / .NET Core / Angular … Net Full Stack Developer to be based at their offices in Reigate. Role & Responsibilities: … casual dress code • Free coffee machine and fruit Senior .NET Full Stack",
-			logo:""
+			logo:"https://logo.clearbit.com/company.com"
 		},
 		{
 			title: "Lead NodeJs Developer",
@@ -28,7 +28,7 @@ const jobs = {
 			company: "Acme Company",
 			post_date: "14 days ago",
 			description: "High-quality Lead Node.js Developer required for tech-driven Green Energy organisation based in Central London The sucessful Node.js Developer will be working on a greenfield project, heading up a small, Agile team of NodeJS Developers, ranging from Junior level to Senior, while remaining ~50% hands-on in terms of coding. The successful candidate will be building a modern, next-gen, highly scalable Green Energy product focused on consumer fairness, sustainability and carbon neutrality.",
-			logo:""
+			logo:"https://logo.clearbit.com/company.com"
 		},
 		{
 			title: "React Developer",
@@ -38,7 +38,7 @@ const jobs = {
 			company: "Acme Company",
 			post_date: "14 days ago",
 			description: "React Developer Circa £75,000 City of London - Marylebone (NW1) ? ShortList Recruitment … The React Developer will work on a re-platforming of existing products to take advantage … The React Developer will benefit from working in a purely greenfield project",
-			logo:""
+			logo:"https://logo.clearbit.com/company.com"
 		},
 		{
 			title: "Senior Developer",
@@ -48,7 +48,7 @@ const jobs = {
 			company: "Acme Company",
 			post_date: "14 days ago",
 			description: "Senior .NET Full Stack Developer - Reigate - Insurance - Azure Cloud / .NET Core / Angular … Net Full Stack Developer to be based at their offices in Reigate. Role & Responsibilities: … casual dress code • Free coffee machine and fruit Senior .NET Full Stack",
-			logo:""
+			logo:"https://logo.clearbit.com/company.com"
 		},
 		{
 			title: "Lead NodeJs Developer",
@@ -58,7 +58,7 @@ const jobs = {
 			company: "Acme Company",
 			post_date: "14 days ago",
 			description: "High-quality Lead Node.js Developer required for tech-driven Green Energy organisation based in Central London The sucessful Node.js Developer will be working on a greenfield project, heading up a small, Agile team of NodeJS Developers, ranging from Junior level to Senior, while remaining ~50% hands-on in terms of coding. The successful candidate will be building a modern, next-gen, highly scalable Green Energy product focused on consumer fairness, sustainability and carbon neutrality.",
-			logo:""
+			logo:"https://logo.clearbit.com/company.com"
 		},
 	]
 };
